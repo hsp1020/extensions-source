89a2c532d453a78a13e262d2f21b3e7468b2a335
hsp1020 <125277818+hsp1020@users.noreply.github.com>
2026-07-14 17:55:56 +0900

    Refactor build.gradle.kts for Naver Comic
    Removed the keiyoushi configuration and replaced it with ext properties for Naver Comic.

--- src/ko/navercomic/build.gradle.kts
+++ src/ko/navercomic/build.gradle.kts
@@ -1,30 +1,8 @@
-import io.github.keiyoushi.gradle.api.ContentWarning
-
-plugins {
-    alias(kei.plugins.extension)
+ext {
+    extName = 'Naver Comic'
+    extClass = '.NaverComic'
+    extVersionCode = 8
+    isNsfw = false
 }
 
-keiyoushi {
-    name = "Naver Comic"
-    versionCode = 8
-    contentWarning = ContentWarning.SAFE
-    libVersion = "1.4"
-
-    source {
-        name = "Naver Webtoon"
-        lang = "ko"
-        baseUrl = "https://comic.naver.com"
-    }
-
-    source {
-        name = "Naver Webtoon Best Challenge"
-        lang = "ko"
-        baseUrl = "https://comic.naver.com"
-    }
-
-    source {
-        name = "Naver Webtoon Challenge"
-        lang = "ko"
-        baseUrl = "https://comic.naver.com"
-    }
-}
+apply plugin: "kei.plugins.extension.legacy"
